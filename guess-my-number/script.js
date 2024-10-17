e6e5d32ae1385d17d848caf017017aff8b0a191d
akcinar.omer <akcinardev@gmail.com>
2024-10-17 10:38:51 +0200

    guess number logic implemented

---
+++ guess-my-number/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const messageEl = document.querySelector('.message');
+const scoreEl = document.querySelector('.score');
+const bodyEl = document.querySelector('body');
+const highscoreEl = document.querySelector('.highscore');
+const guessEl = document.querySelector('.guess');
+let againBtn = document.querySelector('.again');
+
+let score = 20;
+let highscore = 0;
+
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function decreaseScore() {
+  score--;
+  scoreEl.textContent = score;
+}
+
+function gameWon() {
+  bodyEl.style.backgroundColor = '#60b347';
+  numberEl.style.width = '30rem';
+  updateHighscore(score);
+}
+
+function updateHighscore(score) {
+  if (score > highscore) {
+    highscoreEl.textContent = score;
+  }
+}
+
+function gameReset() {
+  bodyEl.style.backgroundColor = '#222';
+  numberEl.style.width = '15rem';
+  secretNum = getRandomNum(1, 20);
+  numberEl.textContent = secretNum;
+  score = 20;
+  scoreEl.textContent = 20;
+  guessEl.value = '';
+}
+
+againBtn.addEventListener('click', gameReset);
+
+let secretNum = getRandomNum(1, 20);
+
+const numberEl = document.querySelector('.number');
+numberEl.textContent = secretNum;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(guessEl.value);
+
+  if (!guess) {
+    messageEl.textContent = 'Invalid Number';
+  } else if (guess === secretNum) {
+    messageEl.textContent = 'Correct Number';
+    gameWon();
+  } else if (guess > secretNum) {
+    if (score > 0) {
+      messageEl.textContent = 'Too High';
+      decreaseScore();
+    } else {
+      messageEl.textContent = 'You lost the game';
+    }
+  } else if (guess < secretNum) {
+    if (score > 0) {
+      messageEl.textContent = 'Too Low';
+      decreaseScore();
+    } else {
+      messageEl.textContent = 'You lost the game';
+    }
+  }
+});
