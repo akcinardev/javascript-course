0f25f72a0c0b82ecf410bc279121a95710ffbc01
akcinar.omer <akcinardev@gmail.com>
2024-11-06 11:42:10 +0100

    map on click pin added

--- mapty/script.js
+++ mapty/script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map;
+let mapEvent;
+
 navigator.geolocation.getCurrentPosition(
   position => {
     const { latitude } = position.coords;
@@ -18,16 +21,38 @@ navigator.geolocation.getCurrentPosition(
 
     const coords = [latitude, longitude];
 
-    var map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(coords).addTo(map).bindPopup('Your Location').openPopup();
+    map.on('click', mapE => {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    });
   },
   () => {
     alert('You need to allow location.');
   }
 );
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
